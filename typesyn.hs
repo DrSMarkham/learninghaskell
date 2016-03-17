291042ae0b157032922f7b036a0f72c7e8553253
DrSmarkham <smarkham@live.co.uk>
2016-03-17 23:56:26 +0000

    Added files via upload

---
+++ typesyn.hs
@@ -0,0 +1,54 @@
+type String = [Char]
+
+phoneBook :: [(String,String)]  
+phoneBook =      
+    [("betty","555-2938")     
+    ,("bonnie","452-2928")     
+    ,("patsy","493-2928")     
+    ,("lucille","205-2928")     
+    ,("wendy","939-8282")     
+    ,("penny","853-2492")     
+    ]   
+
+type PhoneBook = [(String,String)]   
+
+type PhoneNumber = String  
+type Name = String  
+type PhoneBook = [(Name,PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool  
+inPhoneBook name pnumber pbook = (name,pnumber) `elem` pbook 
+
+type AssocList k v = [(k,v)] 
+
+type IntMap v = Map Int v  
+
+type IntMap = Map Int
+
+data Either a b = Left a | Right b deriving (Eq, Ord, Read, Show)  
+
+import qualified Data.Map as Map  
+  
+data LockerState = Taken | Free deriving (Show, Eq)  
+  
+type Code = String  
+  
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code  
+lockerLookup lockerNumber map =   
+    case Map.lookup lockerNumber map of   
+        Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist!"  
+        Just (state, code) -> if state /= Taken   
+                                then Right code  
+                                else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"  
+
+lockers :: LockerMap  
+lockers = Map.fromList   
+    [(100,(Taken,"ZD39I"))  
+    ,(101,(Free,"JAH3I"))  
+    ,(103,(Free,"IQSA9"))  
+    ,(105,(Free,"QOTSA"))  
+    ,(109,(Taken,"893JJ"))  
+    ,(110,(Taken,"99292"))  
+    ]  
